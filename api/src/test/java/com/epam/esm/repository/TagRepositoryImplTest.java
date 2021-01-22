201e5ea230827c483b695e34b1bee8895170d943
IvanVarabei <garlic9991@gmail.com>
2021-01-22 18:38:37 +0300

    run integration test

--- api/src/test/java/com/epam/esm/repository/TagRepositoryImplTest.java
+++ api/src/test/java/com/epam/esm/repository/TagRepositoryImplTest.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
